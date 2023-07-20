4320d69bfbace2520ecd31d7d18db811c5181db4
rypred <ryandpreddy@gmail.com>
2023-07-19 20:38:16 -0700

    Added helper function to randomize weather

--- Program.cs
+++ Program.cs
@@ -1,57 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter Season Below:");
-
-string season = Console.ReadLine();
+﻿
 
 // I don't like that I have the random in under every case? can I make this cleaner
-switch(season)
+
+for (int loopIndex = 0; loopIndex < 3; loopIndex++)
 {
-    case "Spring":
-        Random rnd1 = new Random();
-        int weather = rnd1.Next(1, 100);
-        Console.WriteLine(weather);
-        switch (weather)
-            {
-            case int i when i < 2:
-                    Console.WriteLine("Blizzard");
-                 break;
-            case int i when i >= 3 && i <= 20:
-                    Console.WriteLine("Snow/Rain");
-                break;
-            case int i when i >= 21 && i <= 30:
-                Console.WriteLine("Freezing Cold");
-                break;
-            case int i when i >= 31 && i <= 40:
-                Console.WriteLine("Heavy Clouds");
-                break;
-            case int i when i >= 41 && i <= 60:
-                Console.WriteLine("Light Clouds");
-                break;
-            case int i when i >= 61 && i <= 99:
-                Console.WriteLine("Clear Skies");
-                break;
-            case int i when i == 3 :
-                Console.WriteLine("Strange Phenomena");
-                break;
-        }
-    break;
-    case "Summer":
-        Random rnd2 = new Random();
-        int weather2 = rnd2.Next(1, 100);
-        Console.WriteLine(weather2);
-        break;
-    case "Fall":
-        Random rnd3 = new Random();
-        int weather3 = rnd3.Next(1, 100);
-        Console.WriteLine(weather3);
-        break;
-    case "Winter":
-        Random rnd4 = new Random();
-        int weather4 = rnd4.Next(1, 100);
-        Console.WriteLine(weather4);
-        break;
+    Console.WriteLine("Enter Season Below or Type Quit:");
+
+    string season = Console.ReadLine();
+    switch (season)
+    {
+        case "Winter":
+            Random rnd1 = new Random();
+            int weather = rnd1.Next(1, 100);
+            randomizeWeather(weather,"Blizzard", "Snow/Rain", "Freezing Cold","Heavy Clouds", "Light Clouds","Clear Skies", "Strange Phenomena");
+            break;
+        case "Summer":
+            Random rnd2 = new Random();
+            int weather2 = rnd2.Next(1, 100);
+            randomizeWeather(weather2, "Thunderstorm", "Rain", "Light Clouds", "Clear Skies", "High Winds", "Scorching Heat", "Strange Phenomena");
+            break;
+        case "Fall":
+            Random rnd3 = new Random();
+            int weather3 = rnd3.Next(1, 100);
+            break;
+        case "Spring":
+            Random rnd4 = new Random();
+            int weather4 = rnd4.Next(1, 100);
+            break;
+    }
 }
 
-
-Console.ReadLine();
+void randomizeWeather(int weatherNum, string weatherType1, string weatherType2, string weatherType3, string weatherType4, string weatherType5, string weatherType6, string weatherType7)
+{
+    Console.WriteLine("Called the Randomize Weather Function");
+    switch (weatherNum)
+    {
+        case int i when i < 2:
+            Console.WriteLine(weatherType1);
+            break;
+        case int i when i >= 3 && i <= 20:
+            Console.WriteLine(weatherType2);
+            break;
+        case int i when i >= 21 && i <= 30:
+            Console.WriteLine(weatherType3);
+            break;
+        case int i when i >= 31 && i <= 40:
+            Console.WriteLine(weatherType4);
+            break;
+        case int i when i >= 41 && i <= 60:
+            Console.WriteLine(weatherType5);
+            break;
+        case int i when i >= 61 && i <= 99:
+            Console.WriteLine(weatherType6);
+            break;
+        case int i when i == 100:
+            Console.WriteLine(weatherType7);
+            break;
+    }
+}
 
